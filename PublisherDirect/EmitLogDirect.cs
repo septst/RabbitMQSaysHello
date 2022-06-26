4d5fb719924857404b34d01397d6a3a62f8ace6c
septst <parthibansekar87@gmail.com>
2022-06-26 10:52:21 +0100

    Added code for direct exchange with routing key

---
+++ PublisherDirect/EmitLogDirect.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var exchangeName = "direct_logs";
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchangeName,
+    ExchangeType.Direct
+);
+
+var inputs = args;
+
+do
+{
+    var severity = GetSeverity(inputs);
+    var message = GetMessage(inputs);
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchangeName,
+        severity,
+        null,
+        body
+    );
+
+    Console.WriteLine($"[x] sent {severity}: {message} message.");
+    Console.WriteLine("[x] Please type another message or no to quit sending messages.");
+    inputs = Console.ReadLine()?.Split(" ");
+} while (inputs?.Length > 0 &&
+         !inputs.Contains("no"));
+
+
+static string GetSeverity(string[] args)
+{
+    return args.Length > 0
+        ? args[0]
+        : "info";
+}
+
+static string GetMessage(string[] args)
+{
+    return (args.Length > 1)
+        ? string.Join(" ", args.Skip(1).ToArray())
+        : "Hello World!";
+}
---
+++ SubscriberDirect/ReceiveLogDirect.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var exchangeName = "direct_logs";
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchangeName,
+    ExchangeType.Direct
+);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+        Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+foreach (var severity in args)
+{
+    // queueName = channel.QueueDeclare().QueueName;
+
+    channel.QueueBind(
+        queueName,
+        exchangeName,
+        severity
+    );
+}
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var routingKey = ea.RoutingKey;
+    Console.WriteLine(" [x] Received '{0}':'{1}'",
+        routingKey, message);
+
+    channel.BasicAck(
+        ea.DeliveryTag,
+        false);
+    
+    Console.WriteLine(" Press any key to exit.");
+};
+
+channel.BasicConsume(
+    queueName,
+    false,
+    consumer);
+
+Console.WriteLine(" Press any key to exit.");
+Console.ReadKey();
